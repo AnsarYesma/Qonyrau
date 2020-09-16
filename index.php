a70e0e1a7e95185fb769529321e840f848c8dc7c
Ansar <ansar@MacBook-Pro.local>
2020-09-16 23:34:41 +0600

    create

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   <link rel="stylesheet" href="stylesheets/today.css">
   <link rel="stylesheet" href="stylesheets/head.css">
 	<link rel="script" type="text/javascript" href="scripts/script.php">
+	<link rel="shortcut icon" href="icon.jpg">
   <!-- <link rel="icon" type="image" href="images/interface.png"> -->
 </head>
 
