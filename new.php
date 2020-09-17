552f62988f722885844f04e0c58f0badeb72f4a6
Ansar <ansar@MacBook-Pro.local>
2020-09-17 09:38:36 +0600

    ck

--- new.php
+++ new.php
@@ -4,19 +4,21 @@
   $data = $_POST['data'];
   $myread = fopen('in.txt', 'w+');
 	fwrite($myread, $data);
-  echo $data;
+  // echo $data;
   fseek($myread, 0);
   // $myread = fopen('in.txt', 'r');
   for ( $i = 1; $i <= 5; $i++ ) {
      $lsCnt = fgets($myread);
      // $lsCnt = substr($lsCnt, 0, -1);
+     echo $lsCnt." ";
      // $query = mysqli_query($conn, "insert lessCnt(className, weekDay, count) values('". $classid ."', ". $i .", ". $lsCnt .");");
      // echo "insert lessCnt(className, weekDay, count) values('". $classid ."', ". $i .", ". $lsCnt ."); \n";
      for ( $j = 1; $j <= $lsCnt; $j++ ) {
        $lsName = fgets($myread);
+       echo $lsName." ";
        // $lsName = substr($lsName, 0, -1);
        $query = mysqli_query($conn, "insert keste(className, weekDay, lessId, lessName) values('". $classid ."', ". $i .", ". $j .", '". $lsName ."');");
-       echo $lsName . " ";
+       // echo $lsName . " ";
      }
    }
    // header ('Location: action.php?class='.$classid .'');
