12aeb49f5ada5f27af0c152736a556f14020b1cb
Ansar <ansar@MacBook-Pro.local>
2020-09-17 18:45:44 +0600

    fixed

--- new.php
+++ new.php
@@ -2,19 +2,20 @@
   include "connect.php";
   $classid = $_POST['name'];
   $string = $_POST['data'];
-  $data = explode('\n', $string);
+  $data = explode("\n", $string);
   $k = 0;
+  echo $classid;
   for ( $i = 1; $i <= 5; $i++ ) {
      $lsCnt = $data[$k];
-     echo $lsCnt . " ";
+     // echo $lsCnt . " ";
      $k++;
      for ( $j = 1; $j <= $lsCnt; $j++ ) {
        $lsName = $data[$k];
        $k++;
-       echo $lsName." ";
+       // echo $lsName." ";
        // $lsName = substr($lsName, 0, -1);
        $query = mysqli_query($conn, "insert keste(className, weekDay, lessId, lessName) values('". $classid ."', ". $i .", ". $j .", '". $lsName ."');");
-       // echo "insert keste(className, weekDay, lessId, lessName) values('". $classid ."', ". $i .", ". $j .", '". $lsName ."');";
+       echo "insert keste(className, weekDay, lessId, lessName) values('". $classid ."', ". $i .", ". $j .", '". $lsName ."'); \n";
      }
    }
    // header ('Location: action.php?class='.$classid .'');
